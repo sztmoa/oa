ff1168269224d55f2666e0d88c0daefe70eb5aed
x55756016@gmail.com <x55756016@gmail.com@3097187a-25b6-9e1c-3d28-c22669486c44>
2013-12-13 11:47:33 +0000

    出差方案增加岗位无补贴设置

---
+++ SMT.SaaS.OANew - 航信/SMT_OA_EFModel/生成SMT_OA_EFModel.bat
@@ -0,0 +1,10 @@
+cd C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727
+c:
+Edmgen.exe /provider:EFOracleProvider /mode:fullgeneration /connectionstring:"data source=SMTSAAS94;user id=smtoa; password=test" /project:SMT_OA_EFModel
+
+cd C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727
+c:
+
+start C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727
+
+pause
