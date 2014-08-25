c982b587474b1ff44e33f8483d21b9959a4977c4
x55756016 <55756016@qq.com>
2014-08-25 16:37:03 +0800

    hangxinClockImportTool

---
+++ 源码/autoImpotCard_Hanwang/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace AttendRecordImportHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtRecord = new System.Windows.Forms.DataGridView();
+            this.btnget = new System.Windows.Forms.Button();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.dtFrom = new System.Windows.Forms.DateTimePicker();
+            this.dtTo = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dtRecord)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dtRecord
+            // 
+            this.dtRecord.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtRecord.Location = new System.Drawing.Point(34, 86);
+            this.dtRecord.Name = "dtRecord";
+            this.dtRecord.RowTemplate.Height = 23;
+            this.dtRecord.Size = new System.Drawing.Size(666, 356);
+            this.dtRecord.TabIndex = 0;
+            // 
+            // btnget
+            // 
+            this.btnget.Location = new System.Drawing.Point(416, 16);
+            this.btnget.Name = "btnget";
+            this.btnget.Size = new System.Drawing.Size(250, 23);
+            this.btnget.TabIndex = 1;
+            this.btnget.Text = "从打卡机获取数据";
+            this.btnget.UseVisualStyleBackColor = true;
+            this.btnget.Click += new System.EventHandler(this.btnget_Click);
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(450, 462);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(250, 23);
+            this.btnImport.TabIndex = 2;
+            this.btnImport.Text = "上传至协同办公系统";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // dtFrom
+            // 
+            this.dtFrom.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.dtFrom.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtFrom.Location = new System.Drawing.Point(29, 19);
+            this.dtFrom.Name = "dtFrom";
+            this.dtFrom.Size = new System.Drawing.Size(166, 21);
+            this.dtFrom.TabIndex = 3;
+            // 
+            // dtTo
+            // 
+            this.dtTo.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.dtTo.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtTo.Location = new System.Drawing.Point(224, 18);
+            this.dtTo.Name = "dtTo";
+            this.dtTo.Size = new System.Drawing.Size(167, 21);
+            this.dtTo.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.dtTo);
+            this.groupBox1.Controls.Add(this.dtFrom);
+            this.groupBox1.Controls.Add(this.btnget);
+            this.groupBox1.Location = new System.Drawing.Point(34, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(666, 52);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "获取指定时间段打卡记录";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "从";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "至";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 508);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.dtRecord);
+            this.Name = "Form1";
+            this.Text = "打卡导入工具";
+            ((System.ComponentModel.ISupportInitialize)(this.dtRecord)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtRecord;
+        private System.Windows.Forms.Button btnget;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.DateTimePicker dtFrom;
+        private System.Windows.Forms.DateTimePicker dtTo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 源码/autoImpotCard_Hanwang/Form1.cs
@@ -0,0 +1,137 @@
+﻿using AttendRecordImportHelper.AttendanceWS;
+using SMT.Foundation.Log;
+using SMT.HRM.ImportAttRecordWinSV;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AttendRecordImportHelper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            dtFrom.Text = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day).AddDays(-1).ToString("yyyy-MM-dd HH:mm:ss");
+            dtTo.Text = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day).AddSeconds(-1).ToString("yyyy-MM-dd HH:mm:ss");
+
+        }
+
+        List<T_HR_EMPLOYEECLOCKINRECORD> entTempList = new List<T_HR_EMPLOYEECLOCKINRECORD>();
+        private AttendRecordImportHelper.AttendanceWS.AttendanceServiceClient clientAtt;
+        string TestMode = ConfigurationManager.AppSettings["TestMode"].ToString();
+        public void GetAttendRecord()
+        {
+            try { 
+
+            if (clientAtt == null) clientAtt = new AttendRecordImportHelper.AttendanceWS.AttendanceServiceClient();
+
+            DateTime From = new DateTime();
+            DateTime To = new DateTime();
+            DateTime.TryParse(dtFrom.Text, out From);
+            DateTime.TryParse(dtTo.Text, out To);
+
+            entTempList.Clear();
+
+            using (hrEntities db = new hrEntities())
+            {
+
+                var data = from ent in db.kqjl
+                           where ent.checktime >= From
+                           && ent.checktime <= To
+                           select ent;
+                foreach (var item in data)
+                {
+                    T_HR_EMPLOYEECLOCKINRECORD entTemp = new T_HR_EMPLOYEECLOCKINRECORD();
+                    entTemp.CLOCKINRECORDID = System.Guid.NewGuid().ToString().ToUpper();
+                    entTemp.FINGERPRINTID = item.badgenumber;
+                    entTemp.CLOCKID = item.machine_sn.ToString();
+                    entTemp.PUNCHDATE = item.checktime;
+                    entTemp.PUNCHTIME = item.checktime.Value.Hour.ToString() + ":" + item.checktime.Value.Minute.ToString();
+                    entTempList.Add(entTemp);
+                }
+
+
+            }
+            }catch(Exception ex)
+            {
+                Tracer.Debug("导入打卡记录失败，失败原因为：" + ex.ToString());
+            }
+
+
+        }
+
+        private void btnget_Click(object sender, EventArgs e)
+        {
+            btnget.Enabled = false;
+            try
+            {
+                GetAttendRecord();
+                dtRecord.DataSource = entTempList;
+            }
+            catch (Exception ex)
+            {
+                Tracer.Debug(ex.ToString());
+                MessageBox.Show("获取失败" + ex.ToString());
+            }
+            finally
+            {
+                btnget.Enabled = true;
+            }
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                DateTime From = new DateTime();
+                DateTime To = new DateTime();
+                DateTime.TryParse(dtFrom.Text, out From);
+                DateTime.TryParse(dtTo.Text, out To);
+
+                DialogResult re = MessageBox.Show("是否导入" + From.ToString("yyyy-MM-dd HH:mm:ss") + "-"
+                    + To.ToString("yyyy-MM-dd HH:mm:ss") + " 的打卡记录至协同办公系统？", "确认对话框", MessageBoxButtons.YesNo);
+                if (re == DialogResult.No)
+                {
+                    return;
+                }
+
+                Tracer.Debug("航信获取打卡记录成功，下载记录数：" + entTempList.Count());
+                string strMsg = string.Empty;
+                string[] companyIds = ConfigurationManager.AppSettings["HangXingcompanyID"].Split(',');
+                foreach (var strCompanyId in companyIds)
+                {
+                    if (TestMode == "true")
+                    {
+                        foreach (var ent in entTempList)
+                        {
+                            Tracer.Debug("员工指纹编码：" + ent.FINGERPRINTID + " 打卡时间：" + ent.PUNCHDATE + ":" + ent.PUNCHTIME);
+                        }
+                    }
+                    else
+                    {
+                        string strCurIP = "数据库读取";
+
+                        clientAtt.ImportClockInRdListByWSRealTime(strCompanyId, entTempList.ToArray(), From, To, strCurIP, ref strMsg);
+                    }
+                    Tracer.Debug("导入打卡记录成功，导入的公司ID为：" + strCompanyId);
+                }
+                MessageBox.Show("导入打卡记录成功!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("导入打卡记录失败，失败原因为：" + ex.ToString());
+                Tracer.Debug("导入打卡记录失败，失败原因为：" + ex.ToString());
+            }
+        }
+    }
+}
