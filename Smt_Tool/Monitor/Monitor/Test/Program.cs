d0400184c2e3bc562960d72891504944f048e1c3
x55756016 <55756016@qq.com>
2014-12-15 17:36:14 +0800

    gift

---
+++ Smt_Tool/Monitor/Monitor/Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ActivityMonitor monitor = new ActivityMonitor();
+            monitor.ActivedTimeOut = 2000;// 2 秒钟超时
+            monitor.TimerInterval = 1000;//1秒钟检查一次
+            monitor.OnSleep += delegate { Console.WriteLine("无操作，等待中......"); };
+            monitor.OnActive += delegate { Console.WriteLine("已捕捉到输入"); };
+
+            monitor.Start();
+            Console.ReadLine();
+        }
+    }
+}
