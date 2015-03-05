b3df968644efa8d01cf477ab4ae7e83f689f76de
x55756016 <55756016@qq.com>
2015-03-05 14:23:11 +0800

    urltool

---
+++ Smt_Tool/URLTool/URLTOOL/URLTOOL/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace URLTOOL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelMsg = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(611, 433);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 20);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "选择文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelMsg
+            // 
+            this.labelMsg.AutoSize = true;
+            this.labelMsg.Location = new System.Drawing.Point(8, 33);
+            this.labelMsg.Name = "labelMsg";
+            this.labelMsg.Size = new System.Drawing.Size(41, 12);
+            this.labelMsg.TabIndex = 1;
+            this.labelMsg.Text = "label1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(307, 446);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(4, 6);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(604, 21);
+            this.txtUrl.TabIndex = 5;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panel4);
+            this.splitContainer1.Panel1.Controls.Add(this.panel3);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.panel2);
+            this.splitContainer1.Panel2.Controls.Add(this.panel1);
+            this.splitContainer1.Size = new System.Drawing.Size(922, 491);
+            this.splitContainer1.SplitterDistance = 307;
+            this.splitContainer1.TabIndex = 6;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.dataGridView1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(0, 45);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(307, 446);
+            this.panel4.TabIndex = 6;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(307, 45);
+            this.panel3.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.webBrowser1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 58);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(611, 433);
+            this.panel2.TabIndex = 7;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labelMsg);
+            this.panel1.Controls.Add(this.txtUrl);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(611, 58);
+            this.panel1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(922, 491);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label labelMsg;
+        private System.Windows.Forms.TextBox txtUrl;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ Smt_Tool/URLTool/URLTOOL/URLTOOL/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Web;
+using System.Windows.Forms;
+
+namespace URLTOOL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int indexNum = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            indexNum = 0;
+            string yearStr = DateTime.Now.Year.ToString()+"-";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new
+                   System.IO.StreamReader(openFileDialog1.FileName);
+                //MessageBox.Show(sr.ReadToEnd());
+
+
+                List<URLString> listAll = new List<URLString>();
+                while (!sr.EndOfStream)
+                {
+
+                    URLString obj = new URLString();
+                    string valuer = sr.ReadLine();
+                    if (valuer.Contains(yearStr))
+                    {
+                        int start = valuer.IndexOf(yearStr);
+                        string k = valuer.Substring(start, 19);
+                        obj.DataTimeString = k;
+                        if(!valuer.Contains("访问网址"))
+                        {
+                            string valuer2 = sr.ReadLine();
+                            valuer = valuer + valuer2;
+                        }
+
+                        if (valuer.Contains("访问网址:"))
+                        {
+                            string[] urlall = valuer.Split('问');
+                            if (urlall.Length > 1)
+                            {
+                                string url = urlall[1];
+                                string strk = url.Substring(url.Length - 1, 1);
+                                if (strk == ".")
+                                {
+                                    url = url.Substring(0, url.Length - 1);
+                                    url = url.Substring(3);
+                                    if(url.Contains(".js")
+                                        ||url.Contains(".css"))
+                                    {
+                                        continue;
+                                    }
+                                    obj.URLAdress = url;
+                                    indexNum++;
+                                    obj.Index = indexNum;
+                                    listAll.Add(obj);
+                                }
+                                else
+                                {
+                                    string valuer2 = sr.ReadLine();
+                                    url = url + valuer2;
+                                    url = url.Substring(0, url.Length - 1);
+                                    if (url.Contains("网址"))
+                                    {
+                                        url = url.Substring(3);
+                                    }
+                                    obj.URLAdress = url;
+                                    indexNum++;
+                                    obj.Index = indexNum;
+                                    listAll.Add(obj);
+                                }
+                            }
+                        }
+                    }
+                   
+                }
+                dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+                dataGridView1.DataSource = listAll;
+                sr.Close();
+            }
+        }
+
+        public class URLString
+        {
+            private int index;
+            private string dtstr;
+            private string urlstr;
+            
+            public int Index
+            {
+                get { return index; }
+                set { index = value; }
+            }
+            public string DataTimeString
+            {
+                get { return dtstr; }
+                set { dtstr = value; }
+            }
+            public string URLAdress
+            {
+                get { return urlstr; }
+                set { urlstr = value; }
+            }
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (this.dataGridView1.CurrentRow!=null)
+            {
+                webBrowser1.Stop();
+                string urlstring = this.dataGridView1.CurrentRow.Cells["URLAdress"].Value.ToString();
+
+
+                string urlstr=HttpUtility.UrlDecode(urlstring);
+                txtUrl.Text = urlstr;
+                webBrowser1.Navigate(urlstring);
+                labelMsg.Text = "开始加载中......";
+            }
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            labelMsg.Text = "已加载完成！";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           //webBrowser1.Document.Encoding = Encoding.UTF8.EncodingName;
+            webBrowser1.ScriptErrorsSuppressed = true;
+        }
+    }
+}
